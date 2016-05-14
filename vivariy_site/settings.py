27240adbbccf2fd192adf2da89ffff20b12c3be9
andrey <andrey@micro-man.com>
2016-05-14 10:43:07 +0000

    some deployment related changes

--- vivariy_site/settings.py
+++ vivariy_site/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'vivariy_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'app', 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'app', 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
