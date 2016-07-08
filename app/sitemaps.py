d9a016ab093bb57f33ecbcdaf364b3ad2f864257
andrey <andrey@micro-man.com>
2016-07-08 10:55:46 +0000

    fix sitemap generation: set site domain hardcoded. Avoiding occasionally switching domain in urls to "example.com"

--- app/sitemaps.py
+++ app/sitemaps.py
@@ -1,10 +1,15 @@
 from django.contrib.sitemaps import Sitemap
 from app import models
 from django.core.urlresolvers import reverse
+from django.contrib.sites.models import Site
 
 
 class StaticViewsMap(Sitemap):
     i18n = True
+
+    def get_urls(self, site=None, **kwargs):
+        site = Site(domain='vivariy.com', name='vivariy.com')
+        return super(StaticViewsMap, self).get_urls(site=site, **kwargs)
 
     def items(self):
         return 'main', 'catalog', 'article_list', 'contacts', 'partners',
@@ -14,6 +19,10 @@ class StaticViewsMap(Sitemap):
 
 
 class CatalogSitemap(Sitemap):
+    def get_urls(self, site=None, **kwargs):
+        site = Site(domain='vivariy.com', name='vivariy.com')
+        return super(CatalogSitemap, self).get_urls(site=site, **kwargs)
+
     def items(self):
         return models.TopCategory.objects.filter(is_hidden=False)
 
@@ -22,6 +31,10 @@ class CatalogSitemap(Sitemap):
 
 
 class ProductsSitemap(Sitemap):
+    def get_urls(self, site=None, **kwargs):
+        site = Site(domain='vivariy.com', name='vivariy.com')
+        return super(ProductsSitemap, self).get_urls(site=site, **kwargs)
+
     def items(self):
         return models.Product.objects.filter(is_hidden=False)
 
@@ -30,6 +43,10 @@ class ProductsSitemap(Sitemap):
 
 
 class ArticlesSitemap(Sitemap):
+    def get_urls(self, site=None, **kwargs):
+        site = Site(domain='vivariy.com', name='vivariy.com')
+        return super(ArticlesSitemap, self).get_urls(site=site, **kwargs)
+
     def items(self):
         return models.Article.objects.filter(is_hidden=False)
 
